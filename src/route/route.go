3de8326e72018c781fbde9b7be7cef2fb9543607
bernhard <bernhard@bernhard-ThinkPad-W520.(none)>
2012-07-30 03:52:59 +0200

    Pulled equal edge and equal cluster case out of rout

--- src/route/route.go
+++ src/route/route.go
@@ -37,144 +37,14 @@ func leg(g *graph.ClusterGraph, waypoints []Point, i int, m graph.Metric, trans 
 	endCoord := geo.Coordinate{Lat: waypoints[i+1][0], Lng: waypoints[i+1][1]}
 	startCluster, startWays := kdtree.NearestNeighbor(startCoord, true /* forward */, trans)
 	endCluster, endWays := kdtree.NearestNeighbor(endCoord, false /* forward */, trans)
-
-	allequal := true
-	oneequal := false
-	if len(startWays) != len(endWays) {
-		allequal = false
-	}
-	for _, startPoint := range startWays {
-		existequal := false
-		for _, endPoint := range endWays {
-			existequal = existequal || (startPoint.Vertex == endPoint.Vertex)
-		}
-		oneequal = oneequal || existequal
-		allequal = allequal && existequal
-	}
-	// Start and Endpoint lie on the same edge
-	if allequal {
-		// Start node == End node
-		if len(startWays) == 1 {
-			polyline := make([]Point, 1)
-			startpoint := Point{startWays[0].Target.Lat, startWays[0].Target.Lng}
-			polyline[0] = startpoint
-			instruction := "Stay where you are" // Mockup describtion
-			step := Step{FormatDistance(0), MockupDuration(0), startpoint, startpoint, polyline, instruction}
-			steps := make([]Step, 1)
-			steps[0] = step
-			return &Leg{FormatDistance(0), MockupDuration(0), startpoint, startpoint, steps}
-		} else { // Start and End node are on the same edge
-			var correctStartWay, correctEndWay graph.Way
-		S:
-			for _, startPoint := range startWays {
-				for _, endPoint := range endWays {
-					if startPoint.Vertex == endPoint.Vertex && (startPoint.Length-endPoint.Length) > 0 {
-						correctStartWay = startPoint
-						correctEndWay = endPoint
-						break S
-					}
-				}
-			}
-			polyline := make([]geo.Coordinate, 0)
-			// Find the steps from start to endpoint
-			startsteps := correctStartWay.Steps
-			if len(startsteps) > 0 && len(correctEndWay.Steps) >= 0 {
-				for i := 0; startsteps[i] != correctEndWay.Steps[len(correctEndWay.Steps)-1]; i++ {
-					polyline = append(polyline, startsteps[i])
-				}
-			} else {
-				// TODO no route was found
-				// It is fine to output an empty polyline at the moment
-			}
-			stepDistance := geo.StepLength(polyline)
-			step := PartwayToStep(polyline, correctStartWay.Target, correctEndWay.Target, stepDistance)
-			steps := make([]Step, 1)
-			steps[0] = step
-			return &Leg{step.Distance, step.Duration, step.StartLocation, step.EndLocation, steps}
-		}
-	} else if oneequal {
-		if len(startWays) == 1 { // If the end node is on the edge outgoing from s
-			var correctEndWay graph.Way
-			for _, i := range endWays {
-				if i.Vertex == startWays[0].Vertex {
-					correctEndWay = i
-					break
-				}
-			}
-			n := len(correctEndWay.Steps)
-			polyline := make([]geo.Coordinate, n)
-			for i, item := range correctEndWay.Steps {
-				polyline[n-i-1] = item
-			}
-			step := PartwayToStep(polyline, startWays[0].Target, correctEndWay.Target, correctEndWay.Length)
-			steps := make([]Step, 1)
-			steps[0] = step
-			return &Leg{step.Distance, step.Duration, step.StartLocation, step.EndLocation, steps}
-		} else if len(endWays) == 1 { // If the start node is on the edge outgoint from e
-			var correctStartWay graph.Way
-			for _, i := range startWays {
-				if i.Vertex == endWays[0].Vertex {
-					correctStartWay = i
-					break
-				}
-			}
-			step := PartwayToStep(correctStartWay.Steps, correctStartWay.Target, endWays[0].Target, correctStartWay.Length)
-			steps := make([]Step, 1)
-			steps[0] = step
-			return &Leg{step.Distance, step.Duration, step.StartLocation, step.EndLocation, steps}
-		} else { // we have s->u->e so they are on adjacent edges.
-			var correctStartWay, correctEndWay graph.Way
-			for _, i := range startWays {
-				for _, j := range endWays {
-					if i.Vertex == j.Vertex {
-						correctStartWay = i
-						correctEndWay = j
-					}
-				}
-			}
-			step1 := PartwayToStep(correctStartWay.Steps, correctStartWay.Target, g.Cluster[startCluster].VertexCoordinate(correctStartWay.Vertex),
-				correctStartWay.Length)
-			step2 := PartwayToStep(correctEndWay.Steps, g.Cluster[endCluster].VertexCoordinate(correctEndWay.Vertex), correctEndWay.Target,
-				correctEndWay.Length)
-			steps := make([]Step, 2)
-			steps[0] = step1
-			steps[1] = step2
-			legDistance := correctStartWay.Length + correctEndWay.Length
-			return &Leg{FormatDistance(legDistance), MockupDuration(legDistance), step1.StartLocation, step2.EndLocation, steps}
-		}
+	
+	if ok,l := edgeLeg(g, startWays, endWays, startCluster, endCluster); ok {
+		return l
 	}
 
 	// Both are in the same Cluster
 	if startCluster == endCluster && startCluster != -1 {
-		router := BidiRouter{Transport: trans, Metric: m}
-		router.Reset(g.Cluster[startCluster])
-		for _, n := range startWays {
-			router.AddSource(n.Vertex, float32(n.Length)) // TODO remove cast
-		}
-		for _, n := range endWays {
-			router.AddTarget(n.Vertex, float32(n.Length)) // TODO remove cast
-		}
-		router.Run()
-		vertices, edges := router.Path()
-		indexstart := -1
-		for i, n := range startWays {
-			if vertices[0] == n.Vertex {
-				indexstart = i
-				break
-			}
-		}
-		indexend := -1
-		for i, n := range endWays {
-			if vertices[len(vertices)-1] == n.Vertex {
-				indexend = i
-				break
-			}
-		}
-		if indexstart != -1 && indexend != -1 {
-			return PathToLeg(g.Cluster[startCluster], float64(router.Distance()), vertices, edges, &startWays[indexstart], &endWays[indexend]) // TODO remove cast
-		} else {
-			panic("Did not find a path between two points in the same cluster.")
-		}
+		return sameCluster(g, startWays, endWays, startCluster, trans, m)
 	} else { //They are in different Clusters or on the overlay graph
 		startRunner := Router{Forward: true, Transport: trans, Metric: m}
 		endRunner := Router{Forward: false, Transport: trans, Metric: m}
@@ -419,4 +289,145 @@ func leg(g *graph.ClusterGraph, waypoints []Point, i int, m graph.Metric, trans 
 // getDistance returns the distance between the two given nodes
 func getDistance(g graph.Graph, v1 graph.Vertex, v2 graph.Vertex) float64 {
 	return g.VertexCoordinate(v1).Distance(g.VertexCoordinate(v2))
+}
+
+// Compute the Leg, when start and endvertex share an edge
+func edgeLeg(g *graph.ClusterGraph, startWays, endWays []graph.Way, startCluster, endCluster int) (bool, *Leg) {
+	allequal := true
+	oneequal := false
+	if len(startWays) != len(endWays) {
+		allequal = false
+	}
+	for _, startPoint := range startWays {
+		existequal := false
+		for _, endPoint := range endWays {
+			existequal = existequal || (startPoint.Vertex == endPoint.Vertex)
+		}
+		oneequal = oneequal || existequal
+		allequal = allequal && existequal
+	}
+	// Start and Endpoint lie on the same edge
+	if allequal {
+		// Start node == End node
+		if len(startWays) == 1 {
+			polyline := make([]Point, 1)
+			startpoint := Point{startWays[0].Target.Lat, startWays[0].Target.Lng}
+			polyline[0] = startpoint
+			instruction := "Stay where you are" // Mockup describtion
+			step := Step{FormatDistance(0), MockupDuration(0), startpoint, startpoint, polyline, instruction}
+			steps := make([]Step, 1)
+			steps[0] = step
+			return true,&Leg{FormatDistance(0), MockupDuration(0), startpoint, startpoint, steps}
+		} else { // Start and End node are on the same edge
+			var correctStartWay, correctEndWay graph.Way
+		S:
+			for _, startPoint := range startWays {
+				for _, endPoint := range endWays {
+					if startPoint.Vertex == endPoint.Vertex && (startPoint.Length-endPoint.Length) > 0 {
+						correctStartWay = startPoint
+						correctEndWay = endPoint
+						break S
+					}
+				}
+			}
+			polyline := make([]geo.Coordinate, 0)
+			// Find the steps from start to endpoint
+			startsteps := correctStartWay.Steps
+			if len(startsteps) > 0 && len(correctEndWay.Steps) >= 0 {
+				for i := 0; startsteps[i] != correctEndWay.Steps[len(correctEndWay.Steps)-1]; i++ {
+					polyline = append(polyline, startsteps[i])
+				}
+			} else {
+				// TODO no route was found
+				// It is fine to output an empty polyline at the moment
+			}
+			stepDistance := geo.StepLength(polyline)
+			step := PartwayToStep(polyline, correctStartWay.Target, correctEndWay.Target, stepDistance)
+			steps := make([]Step, 1)
+			steps[0] = step
+			return true,&Leg{step.Distance, step.Duration, step.StartLocation, step.EndLocation, steps}
+		}
+	} else if oneequal {
+		if len(startWays) == 1 { // If the end node is on the edge outgoing from s
+			var correctEndWay graph.Way
+			for _, i := range endWays {
+				if i.Vertex == startWays[0].Vertex {
+					correctEndWay = i
+					break
+				}
+			}
+			n := len(correctEndWay.Steps)
+			polyline := make([]geo.Coordinate, n)
+			for i, item := range correctEndWay.Steps {
+				polyline[n-i-1] = item
+			}
+			step := PartwayToStep(polyline, startWays[0].Target, correctEndWay.Target, correctEndWay.Length)
+			steps := make([]Step, 1)
+			steps[0] = step
+			return true,&Leg{step.Distance, step.Duration, step.StartLocation, step.EndLocation, steps}
+		} else if len(endWays) == 1 { // If the start node is on the edge outgoint from e
+			var correctStartWay graph.Way
+			for _, i := range startWays {
+				if i.Vertex == endWays[0].Vertex {
+					correctStartWay = i
+					break
+				}
+			}
+			step := PartwayToStep(correctStartWay.Steps, correctStartWay.Target, endWays[0].Target, correctStartWay.Length)
+			steps := make([]Step, 1)
+			steps[0] = step
+			return true, &Leg{step.Distance, step.Duration, step.StartLocation, step.EndLocation, steps}
+		} else { // we have s->u->e so they are on adjacent edges.
+			var correctStartWay, correctEndWay graph.Way
+			for _, i := range startWays {
+				for _, j := range endWays {
+					if i.Vertex == j.Vertex {
+						correctStartWay = i
+						correctEndWay = j
+					}
+				}
+			}
+			step1 := PartwayToStep(correctStartWay.Steps, correctStartWay.Target, g.Cluster[startCluster].VertexCoordinate(correctStartWay.Vertex),
+				correctStartWay.Length)
+			step2 := PartwayToStep(correctEndWay.Steps, g.Cluster[endCluster].VertexCoordinate(correctEndWay.Vertex), correctEndWay.Target,
+				correctEndWay.Length)
+			steps := make([]Step, 2)
+			steps[0] = step1
+			steps[1] = step2
+			legDistance := correctStartWay.Length + correctEndWay.Length
+			return true,&Leg{FormatDistance(legDistance), MockupDuration(legDistance), step1.StartLocation, step2.EndLocation, steps}
+		}
+	}
+	return false,nil
+}
+
+func sameCluster(g *graph.ClusterGraph, startWays, endWays []graph.Way, cluster int, trans graph.Transport, m graph.Metric) *Leg {
+	router := BidiRouter{Transport: trans, Metric: m}
+	router.Reset(g.Cluster[cluster])
+	for _, n := range startWays {
+		router.AddSource(n.Vertex, float32(n.Length)) // TODO remove cast
+	}
+	for _, n := range endWays {
+		router.AddTarget(n.Vertex, float32(n.Length)) // TODO remove cast
+	}
+	router.Run()
+	vertices, edges := router.Path()
+	indexstart := -1
+	for i, n := range startWays {
+		if vertices[0] == n.Vertex {
+			indexstart = i
+			break
+		}
+	}
+	indexend := -1
+	for i, n := range endWays {
+		if vertices[len(vertices)-1] == n.Vertex {
+			indexend = i
+			break
+		}
+	}
+	if indexstart == -1 || indexend == -1 {
+		panic("Did not find a path between two points in the same cluster.")
+	}	
+	return PathToLeg(g.Cluster[cluster], float64(router.Distance()), vertices, edges, &startWays[indexstart], &endWays[indexend]) // TODO remove cast
 }
