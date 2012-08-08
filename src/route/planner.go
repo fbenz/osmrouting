436b48f2dbbd8223701ca3b70b9232fc1c343a21
Steven <s.schaefer89@googlemail.com>
2012-08-08 13:11:09 +0200

    Cleanup planner.go

--- src/route/planner.go
+++ src/route/planner.go
@@ -2,7 +2,6 @@
 package route
 
 import (
-	//"log"
 	"geo"
 	"graph"
 	"kdtree"
@@ -51,7 +50,6 @@ func (r *RoutePlanner) Run() *Result {
 	// Compute the closest point in the graph for each user specified waypoint.
 	count := len(r.Waypoints)
 	r.Locations = make([]kdtree.Location, count)
-	//log.Printf("Planning a route containing %v legs.", count-1)
 	Multiplex(count, r.ConcurrentKd, func (i int) {
 		r.Locations[i] = kdtree.NearestNeighbor(r.Waypoints[i], r.Transport)
 	})
@@ -60,12 +58,10 @@ func (r *RoutePlanner) Run() *Result {
 	// If ConcurrentLegs is set compute the legs concurrently.
 	legs := make([]Leg, count-1)
 	Multiplex(count-1, r.ConcurrentLegs, func (i int) {
-		//log.Printf("ComputeLeg(%v)", i)
 		legs[i] = r.ComputeLeg(i)
 	})
 
 	// Format the results.
-	//log.Printf("Output Results.")
 	distance := 0
 	duration := 0
 	for _, leg := range legs {
@@ -94,20 +90,16 @@ func (r *RoutePlanner) UnionGraph(src, dst kdtree.Location) (*graph.UnionGraph, 
 	srcCluster := -1
 	dstCluster := -1
 	
-	//log.Printf("Create UnionGraph")
-	
 	if src.Cluster != -1 {
 		cluster = append(cluster, r.Graph.Cluster[src.Cluster])
 		indices = append(indices, src.Cluster)
 		srcCluster = 0
-		//log.Printf("Cluster 0: %v\n", src.Cluster)
 	}
 	
 	if dst.Cluster != -1 && src.Cluster != dst.Cluster {
 		cluster = append(cluster, r.Graph.Cluster[dst.Cluster])
 		indices = append(indices, dst.Cluster)
 		dstCluster = srcCluster + 1
-		//log.Printf("Cluster %v: %v\n", dstCluster, dst.Cluster)
 	} else if src.Cluster == dst.Cluster {
 		dstCluster = srcCluster
 	}
@@ -135,34 +127,6 @@ func (r *RoutePlanner) EdgeBetween(g graph.Graph, u, v graph.Vertex) graph.Edge 
 	return minEdge
 }
 
-func (r *RoutePlanner) DartBetween(g graph.Graph, u, v graph.Vertex) float32 {
-	minWeight := float32(math.Inf(1))
-	for _, d := range g.VertexNeighbors(u, true, r.Transport, r.Metric, nil) {
-		if d.Vertex != v {
-			continue
-		}
-		weight := d.Weight
-		if weight < minWeight {
-			minWeight = weight
-		}
-	}
-	return minWeight
-}
-
-func (r *RoutePlanner) ReverseDartBetween(g graph.Graph, u, v graph.Vertex) float32 {
-	minWeight := float32(math.Inf(1))
-	for _, d := range g.VertexNeighbors(v, false, r.Transport, r.Metric, nil) {
-		if d.Vertex != u {
-			continue
-		}
-		weight := d.Weight
-		if weight < minWeight {
-			minWeight = weight
-		}
-	}
-	return minWeight
-}
-
 // Compute one path segment between location[waypointIndex] and location[waypointIndex+1]
 func (r *RoutePlanner) ComputeLeg(waypointIndex int) Leg {
 	src := r.Locations[waypointIndex]
@@ -175,7 +139,6 @@ func (r *RoutePlanner) ComputeLeg(waypointIndex int) Leg {
 	g, srcCluster, dstCluster := r.UnionGraph(src, dst)
 	
 	// Run Dijkstra on the union graph
-	//log.Printf("Dijkstra on the UnionGraph")
 	router := &BidiRouter{
 		Transport: r.Transport,
 		Metric:    r.Metric,
@@ -192,7 +155,6 @@ func (r *RoutePlanner) ComputeLeg(waypointIndex int) Leg {
 	router.Run()
 	
 	// Gather the result path.
-	//log.Printf("Construct the vertex path.")
 	vpath    := router.VPath()
 	segments := [][]Step(nil)
 	sketches := []int(nil)
@@ -212,53 +174,28 @@ func (r *RoutePlanner) ComputeLeg(waypointIndex int) Leg {
 			e := r.EdgeBetween(overlay, u, v)
 			if int(e) == -1 {
 				// Shortcut edge, we will have to elaborate it later.
-				//log.Printf("Shortcut edge %v", len(segments))
-				//log.Printf("From vertex %v to %v", u, v)
-				//log.Printf("Weight:    %v", r.DartBetween(overlay, u, v))
-				//log.Printf("RevWeight: %v", r.ReverseDartBetween(overlay, u, v))
 				sketches = append(sketches, len(segments))
 				indices  = append(indices, i)
 			} else {
 				// Cut edge
-				//log.Printf("Cut edge %v", len(segments))
-				//log.Printf("From vertex %v to %v", u, v)
-				//log.Printf("Weight: %v", overlay.EdgeWeight(e, r.Transport, r.Metric))
 				steps = append(steps, r.EdgeToStep(overlay, e, u, v))
 			}
-			//clusterIndexU, idU := g.Overlay.VertexCluster(u)
-			//log.Printf("ClusterIndex u: %v, in cluster id: %v", clusterIndexU, idU)
-			//clusterIndexV, idV := g.Overlay.VertexCluster(v)
-			//log.Printf("ClusterIndex v: %v, in cluster id: %v", clusterIndexV, idV)
 			i++
 		} else {
 			// A path within a cluster. We collect all edges until we hit the
 			// next boundary vertex.
-			//clusterIndex := -1
-			//if uindex == -1 {
-			//	clusterIndex, _ = g.Overlay.VertexCluster(u)
-			//} else {
-			//	clusterIndex = g.Indices[uindex]
-			//}
-			//log.Printf("Cluster path in cluster %v", clusterIndex)
-			//log.Printf(" * Vertex %v", u)
 			u, cluster := g.ToClusterVertex(u, uindex)
-			//log.Printf("  * Cluster Vertex %v", u)
 			j, done := i + 1, false
 			for !done && j < len(vpath) {
 				v := vpath[j]
 				
 				// Project the vertex v onto the current cluster.
 				vindex := g.VertexToCluster(v)
-				//log.Printf(" * Vertex %v, Index: %v", v, vindex)
 				if vindex == -1 {
 					done = true
-					//var clusterIndexV int
-					/*clusterIndexV*/_, v = g.Overlay.VertexCluster(v)
-					//log.Printf("  * Cluster Vertex: %v, Cluster: %v", v, clusterIndexV)
+					_, v = g.Overlay.VertexCluster(v)
 				} else {
 					v, _ = g.ToClusterVertex(v, vindex)
-					//clusterIndexV := g.Indices[vindex]
-					//log.Printf("  * Cluster Vertex: %v, Cluster: %v", v, clusterIndexV)
 					j++
 				}
 				
@@ -274,7 +211,6 @@ func (r *RoutePlanner) ComputeLeg(waypointIndex int) Leg {
 	}
 	
 	// Elaborate the result path
-	//log.Printf("Elaborate the path.")
 	Multiplex(len(sketches), r.ConcurrentPaths, func (i int) {
 		// Find the boundary vertices and cluster corresponding to this shortcut.
 		index := indices[i]
@@ -282,13 +218,7 @@ func (r *RoutePlanner) ComputeLeg(waypointIndex int) Leg {
 		cluster := r.Graph.Cluster[clusterIndex]
 		_, v := g.Overlay.VertexCluster(vpath[index+1])
 		
-		// Ensure that the vertices are accessible
-		//log.Printf("Routing from vertex %v to %v", u, v)
-		//log.Printf("Access[u]: %v", cluster.VertexAccessible(u, r.Transport))
-		//log.Printf("Access[v]: %v", cluster.VertexAccessible(v, r.Transport))
-		
 		// Run Dijkstra to find a u -> v path.
-		//log.Printf("Run Dijkstra within a cluster.")
 		router := &BidiRouter{
 			Transport: r.Transport,
 			Metric:    r.Metric,
@@ -299,7 +229,6 @@ func (r *RoutePlanner) ComputeLeg(waypointIndex int) Leg {
 		router.Run()
 		
 		// Convert this path into a step array.
-		//log.Printf("Convert to step array.")
 		vertices, edges := router.Path()
 		steps := make([]Step, len(edges))
 		for j, edge := range edges {
@@ -311,7 +240,6 @@ func (r *RoutePlanner) ComputeLeg(waypointIndex int) Leg {
 	})
 	
 	// Build Leg
-	//log.Printf("Build leg.")
 	indexstart, indexend := -1, -1
 	var startc, stopc geo.Coordinate
 	for i, srcWay := range srcWays {
